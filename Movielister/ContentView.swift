13c45f4a87bd077800ad9c22744c2c56da306aa5
Hilal Kahraman <hllkhrmn@hotmail.com>
2025-11-18 22:39:49 +0300

    Initial Commit

---
+++ Movielister/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movielister
+//
+//  Created by Hilal Kahraman on 18.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movielister/MovielisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovielisterApp.swift
+//  Movielister
+//
+//  Created by Hilal Kahraman on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovielisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
