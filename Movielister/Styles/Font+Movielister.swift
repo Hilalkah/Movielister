af8c89c4322985acb754d45b939f2ef1d67d3b3f
Hilal Kahraman <hllkhrmn@hotmail.com>
2025-11-18 23:28:13 +0300

    Add fonts

---
+++ Movielister/Styles/Font+Movielister.swift
@@ -0,0 +1,39 @@
+//
+//  Font+Movielister.swift
+//  Movielister
+//
+//  Created by Hilal Kahraman on 18.11.2025.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    static var mainTitle: Font {
+        .system(size: 24, weight: .semibold)
+    }
+    
+    static var description: Font {
+        .system(size: 12, weight: .regular)
+    }
+    
+    static var mainText: Font {
+        .system(size: 14, weight: .regular)
+    }
+    
+    static var smallText: Font {
+        .system(size: 10, weight: .regular)
+    }
+    
+    static var smallButtonText: Font {
+        .system(size: 12, weight: .bold)
+    }
+    
+    static var smallTitleText: Font {
+        .system(size: 10, weight: .semibold)
+    }
+    
+    static var smallNumberText: Font {
+        .system(size: 8, weight: .semibold)
+    }
+}
